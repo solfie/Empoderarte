02381ba301c39866a13bc15c04c12b0d2fa9824c
solfie <127946864+solfie@users.noreply.github.com>
2024-12-09 21:46:49 -0300

    Delete Empoderarte directory

--- Empoderarte/usuarios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "usuarios"
-
-urlpatterns = [
-    path('cadastro/', views.cadastro_usuario, name='cadastro_usuario'),
-    path('<int:usuario_id>/', views.perfil_usuario, name='perfil_usuario'),
-]
